60052923b3c60e0c8defc43102462cdeceb4040f
karrasmil80 <zupablo3@gmail.com>
2025-04-24 17:58:55 +0200

    Creacion de las data class en el dao

---
+++ src/main/kotlin/org/example/fichanewteam/dao/JugadorEntity.kt
@@ -0,0 +1,39 @@
+package org.example.fichanewteam.dao
+
+import org.example.models.Jugador.Posicion
+
+/**
+ * Clase que representa a un jugador del equipo.
+ *
+ * @param id Identificador único del jugador.
+ * @param nombre Nombre del jugador.
+ * @param apellidos Apellidos del jugador.
+ * @param fechaNacimiento Fecha de nacimiento del jugador en formato YYYY-MM-DD.
+ * @param fechaIncorporacion Fecha de incorporación del jugador al equipo en formato YYYY-MM-DD.
+ * @param salario Salario del jugador.
+ * @param pais País de origen del jugador.
+ * @param rol Rol del jugador en el equipo (en este caso, siempre será "jugador").
+ * @param posicion Posición del jugador en el campo (defensa, centrocampista, delantero, portero).
+ * @param dorsal Número de camiseta del jugador.
+ * @param altura Altura del jugador en metros.
+ * @param peso Peso del jugador en kilogramos.
+ * @param goles Número de goles anotados por el jugador.
+ * @param partidosJugados Número de partidos jugados por el jugador.
+ */
+
+class JugadorEntity(
+    var id: Long,
+    var nombre: String,
+    var apellidos: String,
+    var fechaNacimiento: String,
+    var fechaIncorporacion: String,
+    var salario: Double?,
+    var pais: String,
+    var rol: String,
+    var posicion: Posicion?,
+    var dorsal: Int?,
+    var altura: Double?,
+    var peso: Double?,
+    var goles: Int,
+    var partidosJugados: Int
+)
