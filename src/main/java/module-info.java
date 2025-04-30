df1f9e007a65665bc252d590ffa239bad86c040d
karrasmil80 <zupablo3@gmail.com>
2025-04-30 19:23:22 +0200

    Añadidas varias dependencias al modulo de java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,14 @@
 module org.example.fichanewteam {
     requires javafx.controls;
     requires javafx.fxml;
-    requires kotlinx.serialization.core;
     requires org.jdbi.v3.sqlobject.kotlin;
     requires org.jdbi.v3.sqlobject;
     requires logging.jvm;
     requires kotlin.result.jvm;
     requires org.jdbi.v3.kotlin;
     requires org.jdbi.v3.core;
+    requires com.github.benmanes.caffeine;
+    requires kotlinx.serialization.json;
 
 
     opens org.example.fichanewteam to javafx.fxml;
