e92d07b2601ac0285d05c5dd53d16542f9c5a4b2
LuciaFuertesCruz <lucia.fuertes@alumno.iesluisvives.org>
2025-05-12 20:49:31 +0200

    Comienzo del view model y ajustes view

--- src/main/kotlin/org/example/fichanewteam/plantilla/viewmodel/PlantillaViewModel.kt
+++ src/main/kotlin/org/example/fichanewteam/plantilla/viewmodel/PlantillaViewModel.kt
@@ -4,6 +4,10 @@ import com.github.michaelbull.result.*
 import org.example.fichanewteam.plantilla.service.PlantillaService
 import org.example.fichanewteam.plantilla.storage.PlantillaStorage
 import javafx.beans.property.SimpleObjectProperty
+import org.example.fichanewteam.plantilla.dto.PlantillaDto
+import org.example.fichanewteam.plantilla.error.PlantillaError
+import org.example.fichanewteam.plantilla.models.Plantilla
+import java.io.File
 
 
 class PlantillaViewModel(
@@ -18,7 +22,7 @@ class PlantillaViewModel(
     }
 
     private fun loadTypes() {
-        TODO("Not yet implemented")
+        state.value = state.value.copy(typesPlantilla = TipoFiltro.entries.map { it.value })
     }
 
     private fun loadPlantilla() {
@@ -28,10 +32,31 @@ class PlantillaViewModel(
         }
     }
     private fun updateActualState() {
-
+        // variables de las posibles consultas
+        state.value = state.value.copy(
+            //variables = variables de las consultas
+        )
     }
 
-    data class PlantillaState()
+    fun plantillaFilteredList(tipo: String, nombre: String): List<Plantilla>{
+
+        return TODO("Provide the return value")
+    }
+
+    fun savePlantillaToJson(file:File): Result<Long, PlantillaError> {
+        return storage.storage
+    }
+
+    enum class TipoFiltro(val value: String) {
+        TODOS("Todos/as"), JUGADOR("Jugador: si"), ENTRENADOR("Entrenador: si")
+    }
+
+    data class PlantillaState(
+        val typesPlantilla: List<String> = emptyList(),
+        val plantilla: List<Plantilla> = emptyList(),
+
+        val persona: PlantillaState = PlantillaState(),
+    )
 }
 
 
