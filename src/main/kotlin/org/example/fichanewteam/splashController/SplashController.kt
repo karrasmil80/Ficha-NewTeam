35f9fa85443accb6fff9ed682532e080c62755db
Pablo Zuil <zupablo3@gmail.com>
2025-05-14 11:33:43 +0200

    Empezando con el splash screen

---
+++ src/main/kotlin/org/example/fichanewteam/splashController/SplashController.kt
@@ -0,0 +1,17 @@
+package org.example.fichanewteam.splashController
+
+import javafx.fxml.FXML
+import javafx.scene.control.ProgressBar
+import java.lang.Thread.sleep
+
+class SplashController {
+
+    @FXML
+    lateinit var progressBar: ProgressBar
+
+    val progress : Int = 0
+
+    fun initialize() {
+
+    }
+}
